6e4adde68daf52fe6a806e6dfaef1b7f20536a09
mnajjari49 <mnajjari49@gmail.com>
2018-10-18 21:27:33 +0100

    Add files via upload

---
+++ hide_numpad-10.0.1.0.0/hide_numpad/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Show/Hide NumPad',
+    'version': '2.0.0',
+    'category': 'Point Of Sale',
+    'author': 'Mnajjari',
+    'sequence': 10,
+    'summary': 'Show and Hide NumPadWidget with Animations',
+    'description': "",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/header.xml'
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'application': True,
+    'license': 'OPL-1',
+	'price':10
+	
+}
